bd463175cf5b769b0248f298a6e041ee43200a9e
shaha <shohruh300908@gmail.com>
2024-02-26 19:43:02 +0600

    first_ex

---
+++ lib/main.dart
@@ -0,0 +1,189 @@
+import 'package:covid_19/widgets/counter.dart';
+import 'package:covid_19/widgets/my_header.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:covid_19/constant.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Covid 19',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        fontFamily: "Poppins",
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: kBodyTextColor),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const MyHeader(
+            image: "assets/icons/Drcorona.svg",
+            textTop: "All you need",
+            textBottom: "is stay at home.",
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: const Color(0xFFE5E5E5),
+              ),
+            ),
+            child: Row(children: [
+              SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+              const SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: DropdownButton(
+                  isExpanded: true,
+                  underline: const SizedBox(),
+                  icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                  value: "Japan",
+                  items: ['Japan', 'Canada', 'Italy', 'South Korea']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (value) {},
+                ),
+              ),
+            ]),
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Case Update\n",
+                            style: kTitleTextstyle,
+                          ),
+                          TextSpan(
+                            text: "Newest update to March 28",
+                            style: TextStyle(
+                              color: kTextLightColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    const Text(
+                      "See details",
+                      style: TextStyle(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 4),
+                        blurRadius: 30,
+                        color: kShadowColor,
+                      ),
+                    ],
+                  ),
+                  child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Counter(
+                          color: kInfectedColor,
+                          number: 1046,
+                          title: "Infected",
+                        ),
+                        Counter(
+                          color: kDeathColor,
+                          number: 87,
+                          title: "Deaths",
+                        ),
+                        Counter(
+                          color: kRecovercolor,
+                          number: 46,
+                          title: "Recovered",
+                        ),
+                      ]),
+                ),
+                const SizedBox(height: 20),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Spread of Virus",
+                      style: kTitleTextstyle,
+                    ),
+                    Text(
+                      "See details",
+                      style: TextStyle(
+                          color: kPrimaryColor, fontWeight: FontWeight.w600),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  padding: const EdgeInsets.all(20),
+                  height: 170,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 10),
+                        blurRadius: 30,
+                        color: kShadowColor,
+                      ),
+                    ],
+                  ),
+                  child: Image.asset(
+                    "assets/images/map.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/my_header.dart
@@ -0,0 +1,106 @@
+import 'package:covid_19/constant.dart';
+import 'package:covid_19/widgets/info_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyHeader extends StatelessWidget {
+  final String image;
+  final String textTop;
+  final String textBottom;
+  const MyHeader({
+    super.key,
+    required this.image,
+    required this.textTop,
+    required this.textBottom,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: MyClipper(),
+      child: Container(
+        padding: const EdgeInsets.only(left: 40, top: 50, right: 20),
+        height: 360,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFF3383CD),
+              Color(0xFF11249F),
+            ],
+          ),
+          image: DecorationImage(
+            image: AssetImage("assets/images/virus.png"),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const InfoScreen();
+                        },
+                      ),
+                    );
+                  },
+                  child: SvgPicture.asset("assets/icons/menu.svg")),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: Stack(
+              children: [
+                SvgPicture.asset(
+                  image,
+                  width: 250,
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topCenter,
+                ),
+                Positioned(
+                  top: 20,
+                  left: 130,
+                  child: Text(
+                    "$textTop \n$textBottom",
+                    style: kHeadingTextStyle.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var height = size.height * 0.65;
+    var path = Path();
+    path.lineTo(0, size.height - height * 0.5);
+    path.quadraticBezierTo(
+        size.width * 0.55, size.height * 0.9, size.width, height);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    throw UnimplementedError();
+  }
+}
